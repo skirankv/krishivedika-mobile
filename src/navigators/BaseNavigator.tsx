41f2c5014628644fdb90972ae372d38439a87ffb
skirankv <sbhamidipati@krishivedika.com>
2024-09-24 11:35:54 +0530

    added the error boundary, its fall back component, navigators and all

---
+++ src/navigators/BaseNavigator.tsx
@@ -0,0 +1,16 @@
+import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const BaseNavigator = () => {
+  return (
+    <View>
+      <Text>BaseNavigator</Text>
+    </View>
+  );
+};
+
+export default BaseNavigator;
+
+// async storage
+// redux
+// axios
